63d21ed75ab47579bf219d1e7f1cafc63fa49b2d
alexbowen <alexbowen123@googlemail.com>
2015-11-20 14:52:42 +0000

    added default options

--- tasks/open-url.js
+++ tasks/open-url.js
@@ -1,15 +1,27 @@
 'use strict';
 
-module.exports = function taskFactory (protocol, host, port, page, queryString) {
+module.exports = function taskFactory (options) {
+
+    var extend = require('extend');
+
+    var defaults = {
+        protocol : 'http://',
+        host : '127.0.0.1',
+        port : '8000',
+        page : null,
+        queryString : null
+    };
+
+    extend(true, defaults, options);
 
     return function task () {
 
         var openUrl = require('open');
 
-        var url = protocol + host + ':' + port + '/' + page;
+        var url = options.protocol + options.host + ':' + options.port + '/' + options.page;
 
-        if (queryString) {
-            url += '?' + queryString;
+        if (options.queryString) {
+            url += '?' + options.queryString;
         }
 
         return openUrl(url);
