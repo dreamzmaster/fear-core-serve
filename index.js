8a9500917419c0d9140209e62d424e073395944b
alexbowen <alexbowen123@googlemail.com>
2015-11-20 00:15:49 +0000

    initial setup

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+    serve: require('./tasks/serve')
+};
---
+++ test/example.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('a component under test', function () {
+
+    it('should be unit tested', function() {
+        expect(true).to.be.true;
+    });
+
+});
