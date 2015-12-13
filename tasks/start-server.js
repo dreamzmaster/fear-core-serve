1d0cc57c6744f9cf6a8e96b8c056427fed89ff03
alexbowen <alexbowen123@googlemail.com>
2015-12-13 23:17:29 +0000

    removed hard coded base for core templates

--- tasks/start-server.js
+++ tasks/start-server.js
@@ -2,68 +2,68 @@
 
 module.exports = function taskFactory (host, port, mustacheConfig, channelDefaults, customMiddleware, staticPaths, liveReloadConditions) {
 
-        var connect = require('gulp-connect');
-        var gulpif = require('gulp-if');
-        var query = require('connect-query');
-        var mustache = require('connect-mustache-middleware');
-        var modRewrite = require('connect-modrewrite');
-        var livereload = require('connect-livereload');
-        var middleware = [getLiveReloadMiddleware(), query()];
+    var connect = require('gulp-connect');
+    var gulpif = require('gulp-if');
+    var query = require('connect-query');
+    var mustache = require('connect-mustache-middleware');
+    var modRewrite = require('connect-modrewrite');
+    var livereload = require('connect-livereload');
+    var middleware = [getLiveReloadMiddleware(), query()];
 
-        function getLiveReloadMiddleware() {
-            return gulpif(liveReloadConditions(), livereload());
-        }
+    function getLiveReloadMiddleware() {
+        return gulpif(liveReloadConditions(), livereload());
+    }
 
-        function addMustacheMiddleware() {
+    function addMustacheMiddleware() {
 
-            mustacheConfig.rootDir = staticPaths[0];
+        mustacheConfig.rootDir = staticPaths[0];
 
-            mustacheConfig.templatePathOverides = {
-                'core' : 'app/jspm_components/github/DigitalInnovation/fear-core-app@development'
-            };
-
-            middleware.push(mustache.middleware(mustacheConfig, channelDefaults));
-        }
-
-        function addCustomMiddleware() {
-            for (var m in customMiddleware) {
-                middleware.push(customMiddleware[m]);
-            }
-        }
-
-        function addRewriteMiddleware() {
-
-            var exclude = ['webapp', 'javascript', 'assets', 'components', 'css', 'scripts', 'views'];
-
-            middleware.push(modRewrite([
-                '^\/(' + exclude.join('|') + ')(\/.*)$ /$1$2', //assets
-                '^\/docs\/(.*)$ /generated/$1/index.html [L]', //documentation pages
-                '^\/$ /views/default/pages/home/index.html [L]', //home page
-                '^\/(?!views)([a-zA-Z-_]*)([^.]*)$ /$1/views/default/pages$2/index.html [L]'
-            ]));
-        }
-
-        function addStaticPaths(connect) {
-            for (var p in staticPaths) {
-                middleware.push(connect.static(staticPaths[p]));
-            }
-        }
-
-        function getMiddleware(connect) {
-            addCustomMiddleware();
-            addMustacheMiddleware();
-            addRewriteMiddleware();
-            addStaticPaths(connect);
-
-            return middleware;
-        }
-
-        var opts = {
-            port: port,
-            host: host,
-            middleware: getMiddleware
+        mustacheConfig.templatePathOverides = {
+            'core' : staticPaths[0] + '/jspm_components/github/DigitalInnovation/fear-core-app@app-folder-structure'
         };
 
-        connect.server(opts);
-        return connect;
+        middleware.push(mustache.middleware(mustacheConfig, channelDefaults));
+    }
+
+    function addCustomMiddleware() {
+        for (var m in customMiddleware) {
+            middleware.push(customMiddleware[m]);
+        }
+    }
+
+    function addRewriteMiddleware() {
+
+        var exclude = ['webapp', 'javascript', 'assets', 'components', 'css', 'scripts', 'views'];
+
+        middleware.push(modRewrite([
+            '^\/(' + exclude.join('|') + ')(\/.*)$ /$1$2', //assets
+            '^\/docs\/(.*)$ /generated/$1/index.html [L]', //documentation pages
+            '^\/$ /views/default/pages/home/index.html [L]', //home page
+            '^\/(?!views)([a-zA-Z-_]*)([^.]*)$ /$1/views/default/pages$2/index.html [L]'
+        ]));
+    }
+
+    function addStaticPaths(connect) {
+        for (var p in staticPaths) {
+            middleware.push(connect.static(staticPaths[p]));
+        }
+    }
+
+    function getMiddleware(connect) {
+        addCustomMiddleware();
+        addMustacheMiddleware();
+        addRewriteMiddleware();
+        addStaticPaths(connect);
+
+        return middleware;
+    }
+
+    var opts = {
+        port: port,
+        host: host,
+        middleware: getMiddleware
+    };
+
+    connect.server(opts);
+    return connect;
 };
